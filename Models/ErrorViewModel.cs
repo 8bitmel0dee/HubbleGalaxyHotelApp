ea84f455ac56cbcdd90dc3253612d62f5779f88e
8bitmel0dee <8bitmel0dee@gmail.com>
2020-01-26 21:51:30 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HubbleGalaxyHotelApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
