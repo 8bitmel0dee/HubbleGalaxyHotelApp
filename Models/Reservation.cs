1b9b990e6ad8cac83b7ee2684935da795e6ee550
8bitmel0dee <8bitmel0dee@gmail.com>
2020-02-04 14:44:06 -0500

    Reservation Details - added logic to calculate Total

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace HubbleGalaxyHotelApp.Models
 {
@@ -12,14 +13,19 @@ namespace HubbleGalaxyHotelApp.Models
 		public Room Room { get; set; }
 		
 		public int RoomId { get; set; }
+
+		[Required]
+		[Display(Name = "Select Payment Type")]
 		public int PaymentTypeId { get; set; }
 
 
 		public ApplicationUser ApplicationUser { get; set; }
 		public string UserId { get; set; }
 
-
+		[Display(Name = "Start Date")]
 		public DateTime StartDate { get; set; }
+
+		[Display(Name = "End Date")]
 		public DateTime EndDate { get; set; }
 
 	}
--- Models/ViewModels/CreateReservationViewModel.cs
+++ Models/ViewModels/CreateReservationViewModel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HubbleGalaxyHotelApp.Models.ViewModels
 {
     public class CreateReservationViewModel
     {
-        
+            
             public List<SelectListItem> PaymentTypes { get; set; }
 
             public Reservation Reservation { get; set; }
